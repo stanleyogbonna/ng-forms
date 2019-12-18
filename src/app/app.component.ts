1962473432ece09119ac6bd60607d5877296aeb2
Stanley Ogbonna <ogbonnastanley@gmail.com>
2019-12-18 21:16:44 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-forms';
+  heroInfo() {
+    const newHero = new Hero(34, 'abc', 'def');
+    console.log(newHero)
+  }
 }
